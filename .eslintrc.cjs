c18ef61c615c514a3b810ac79ecffc79bbf64c46
Keith Hartman <khman32@protonmail.com>
2023-01-31 11:06:17 -0500

    Updated formatting of eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,16 +1,22 @@
 module.exports = {
+  root: true,
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended"
+  ],
   env: {
     browser: true,
     es2021: true,
     node: true,
   },
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
-  overrides: [],
   parser: "@typescript-eslint/parser",
+  plugins: [
+    "@typescript-eslint",
+    "prettier"
+  ],
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
+    project: "./tsconfig.json",
   },
-  plugins: ["@typescript-eslint", "prettier"],
-  rules: {},
 };
