8468bf25d883212058b655afcc41c403d4fa25f2
Keith Hartman <khman32@protonmail.com>
2023-01-31 11:09:17 -0500

    Removed old rom detection, updated module exports

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,29 @@
+import { RomType } from "./BaseRom";
+import Rom from "./Rom";
+
 import NesRom from "./NesRom";
 import SnesRom from "./SnesRom";
 import GbRom from "./GbRom";
 import GbaRom from "./GbaRom";
 
+import { PatchType } from "./BasePatch";
+import Patch from "./Patch";
+
 import IpsPatch from "./IpsPatch";
 import UpsPatch from "./UpsPatch";
 import BpsPatch from "./BpsPatch";
 
-import { detectRomType, RomType, bufferToPatch } from "./detect";
-
 export {
+  Rom,
+  RomType,
   NesRom,
   SnesRom,
   GbRom,
   GbaRom,
+
+  Patch,
+  PatchType,
   IpsPatch,
   UpsPatch,
   BpsPatch,
-  detectRomType,
-  bufferToPatch,
-  RomType
 };
